f89bcc9457f031171e772ec5d85a6ac188ff4adf
KrauzerPH94 <pkrauzer94@gmail.com>
2023-08-21 23:04:30 -0300

    Trying to make a simple conditional function

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-// #01 Randomly return a string between: Rock, Paper and Scissors
-function getComputerChoice (choice) {
+// Randomly return a string between: Rock, Paper and Scissors
+function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
     const random = options[Math.floor(Math.random() * options.length)];
     return random;
 }
 
-// Validating #01
+// validating getComputerChoice
 // console.log(getComputerChoice());
 
-// #02 
+// Take player choice and computer choice in two parameters
+function gameMatch(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+}
+
+// Testing a simple prompt return function
+function simpleChoice(choice) {
+    choice = prompt('Choose betwen 1, 2 or 3: ');
+    switch (choice) {
+        case (1):
+            return alert('Too little');
+            break;
+        case (2):
+            return console.log('In between');
+            break;
+        case (3):
+            return console.log('Too much');
+            break;
+    }
+}
+
+simpleChoice();
+
+// Conditionals for the result, there are 3 possible outcomes
+// Return a string of success/failure depending on the choice
