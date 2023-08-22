394ac8aa1b3f732af69c09fc3fb4d60cb439dd44
KrauzerPH94 <pkrauzer94@gmail.com>
2023-08-22 02:17:41 -0300

    Erased the case function, trying with if again

--- script.js
+++ script.js
@@ -6,95 +6,21 @@ function getComputerChoice() {
 }
 
 // Take player choice and computer choice in two parameters
-function gameMatchIf(playerSelection, computerSelection) {
-  // computerSelection = getComputerChoice(); // Random choice
-
-  let outcome = ""; // For later text return
-  playerSelection = prompt("Rock, Paper or Scissors?");
+function playRound(playerSelection, computerSelection) {
+  let outcome; // For later text return
 
   // Conditionals for the result, there are 3 possible outcomes
-  // Failure outcomes
-  if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    outcome = console.log("You lose! Rock beats Scissors");
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    outcome = console.log("You lose! Paper beats Rock");
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    outcome = console.log("You lose! Scissors beats Paper");
+  if (computerSelection == "Scissors") {
+    return (outcome = "You win! Rock beats Scissors");
+  } else if (computerSelection == "Rock") {
+    return (outcome = "Draw");
+  } else if (computerSelection == "Paper") {
+    return (outcome = "You lose! Paper beats Rock");
   }
-  // Success outcomes
-  else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    outcome = console.log("You win! Scissors beats Paper");
-  } else if (playerSelection == "Rock" && computerSelection === "Scissors") {
-    outcome = console.log("You win! Rock beats Scissors");
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    outcome = console.log("You win! Paper beats Rock");
-  }
-
-  return outcome;
 }
 
-// Second atempt, now using case instead of if
-function gameMatchCase(playerSelection2, computerSelection2) {
-  // computerSelection2 = getComputerChoice(); // Random choice
-
-  let outcome2 = ""; // For later text return
-  playerSelection2 = prompt("Rock, Paper or Scissors?");
-
-  // Conditionals for the result, there are 3 possible outcomes
-  switch (playerSelection2) {
-    case "Rock":
-      if (computerSelection2 === "Scissors") {
-        outcome2 = console.log("You win! Rock beats Scissors");
-      } else if (computerSelection2 === "Paper") {
-        outcome2 = console.log("You lose! Paper beats Rock");
-      } else {
-        outcome2 = console.log("Its a draw");
-      }
-      break;
-    case "Paper":
-      if (computerSelection2 === "Rock") {
-        outcome2 = console.log("You win! Paper beats Rock");
-      } else if (computerSelection2 === "Scissors") {
-        outcome2 = console.log("You lose! Scissors beats Paper");
-      } else {
-        outcome2 = console.log("Its a draw");
-      }
-      break;
-    case "Scissors":
-      if (computerSelection2 === "Paper") {
-        outcome2 = console.log("You win! Scissors beats Paper");
-      } else if (computerSelection2 === "Rock") {
-        outcome2 = console.log("You lose! Rock beats Scissors");
-      } else {
-        outcome2 = console.log("Its a draw");
-      }
-      break;
-  }
-
-  return outcome2;
-}
+// Validation using fixed outputs
+const playerSel = "Rock";
+const computerSel = getComputerChoice();
 console.log(getComputerChoice());
-// gameMatchElif();
-// gameMatchCase();
-
-let playerAnswer = outcome2;
-gameMatchCase(playerAnswer, getComputerChoice());
-
-// Simple example function for 3 outcomes
-function simpleChoice(choice) {
-  let text = ""; // New var for the return
-  choice = prompt("1, 2 or 3?");
-  switch (choice) {
-    case "1":
-      text = console.log("AAA");
-      break;
-    case "2":
-      text = console.log("BBB");
-      break;
-    case "3":
-      text = console.log("CCC");
-      break;
-  }
-
-  return text;
-}
+console.log(playRound(playerSel, computerSel));
