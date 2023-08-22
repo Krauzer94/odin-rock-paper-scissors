0c8f5e80fb8592ceee9067828fe1a509a13a6ad0
KrauzerPH94 <pkrauzer94@gmail.com>
2023-08-21 23:35:39 -0300

    Code formating

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Randomly return a string between: Rock, Paper and Scissors
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
-    const random = options[Math.floor(Math.random() * options.length)];
-    return random;
+  let options = ["Rock", "Paper", "Scissors"];
+  const random = options[Math.floor(Math.random() * options.length)];
+  return random;
 }
 
 // validating getComputerChoice
@@ -10,26 +10,26 @@ function getComputerChoice() {
 
 // Take player choice and computer choice in two parameters
 function gameMatch(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
+  computerSelection = getComputerChoice();
 }
 
 // Simple example function for 3 outcomes
 function simpleChoice(choice) {
-    let text = ''; // New var for the return
-    choice = prompt('1, 2 or 3?');
-    switch(choice) {
-        case '1':
-            text = console.log('AAA');
-        break;
-        case '2':
-            text = console.log('BBB');
-        break;
-        case '3':
-            text = console.log('CCC');
-        break;
-    }
+  let text = ""; // New var for the return
+  choice = prompt("1, 2 or 3?");
+  switch (choice) {
+    case "1":
+      text = console.log("AAA");
+      break;
+    case "2":
+      text = console.log("BBB");
+      break;
+    case "3":
+      text = console.log("CCC");
+      break;
+  }
 
-    return text;
+  return text;
 }
 
 // validating simpleChoice;
