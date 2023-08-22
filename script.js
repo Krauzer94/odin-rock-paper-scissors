faf95a2da0e7aa38bca66f09a67522aebde1a07f
KrauzerPH94 <pkrauzer94@gmail.com>
2023-08-21 22:36:19 -0300

    Function to randomly return computer options

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Comments
-console.log("Hello world!");
+// #01 Randomly return a string between: Rock, Paper and Scissors
+function getComputerChoice (choice) {
+    let options = ["Rock", "Paper", "Scissors"];
+    const random = options[Math.floor(Math.random() * options.length)];
+    return random;
+}
+
+// Validating #01
+// console.log(getComputerChoice());
+
+// #02 
