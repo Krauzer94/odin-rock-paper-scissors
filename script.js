a0b7c08519a90d3f056f2dcd334d1c95bcda8f53
KrauzerPH94 <pkrauzer94@gmail.com>
2023-08-24 23:11:08 -0300

    Refactor validation code like console.log()
    getComputerChoice reverted from a function to a const
    This doesn't conflict with TOP's instructions in any way
    Validations like varValue = varValue not removed yet

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Randomly return a string between: Rock, Paper and Scissors
-function getComputerChoice() {
-  let options = ["Rock", "Paper", "Scissors"];
-  const random = options[Math.floor(Math.random() * options.length)];
-  return random;
-}
+// Random choices for the computer to play
+const options = ["Rock", "Paper", "Scissors"];
+const getComputerChoice = () => options[Math.floor(Math.random() * options.length)];
 
 // Take player choice and computer choice in two parameters
 function playRound(playerSelection, computerSelection) {
@@ -43,82 +40,67 @@ function playRound(playerSelection, computerSelection) {
 
 // Find out the winner out of five rounds
 function game(humanScore, machineScore) {
-  let winner = ""; // For later winner return
   let roundCount = 1; // Round loop counter
-  (humanScore = 0), (machineScore = 0); // Scores
+  humanScore = 0, machineScore = 0; // Scores
   let userSelection, randomSelection; // Inputs
 
   do {
     userSelection = prompt("Rock, Paper or Scissors?"); // user input
     randomSelection = getComputerChoice(); // random input
-    console.log(randomSelection); // validation log
-    console.log(playRound(userSelection, randomSelection));
+    // console.log(randomSelection); // validation log
+    // console.log(playRound(userSelection, randomSelection));
     roundCount++;
 
     switch (userSelection) {
       case "Rock":
         if (randomSelection === "Rock") {
           // Its a draw
-          humanScore = humanScore;
-          machineScore = machineScore;
+          humanScore = humanScore, machineScore = machineScore;
         } else if (randomSelection === "Paper") {
           // Computer wins
-          humanScore = humanScore;
           machineScore++;
         } else if (randomSelection === "Scissors") {
           // Player wins
           humanScore++;
-          machineScore = machineScore;
         }
         break;
       case "Paper":
         if (randomSelection === "Rock") {
           // Player wins
           humanScore++;
-          machineScore = machineScore;
         } else if (randomSelection === "Paper") {
           // Its a draw
-          humanScore = humanScore;
-          machineScore = machineScore;
+          humanScore = humanScore, machineScore = machineScore;
         } else if (randomSelection === "Scissors") {
           // Computer wins
-          humanScore = humanScore;
           machineScore++;
         }
         break;
       case "Scissors":
         if (randomSelection === "Rock") {
           // Computer wins
-          humanScore = humanScore;
           machineScore++;
         } else if (randomSelection === "Paper") {
           // Player wins
           humanScore++;
-          machineScore = machineScore;
         } else if (randomSelection === "Scissors") {
           // Its a draw
-          humanScore = humanScore;
-          machineScore = machineScore;
+          humanScore = humanScore, machineScore = machineScore;
         }
         break;
     }
   } while (roundCount <= 5);
 
+  let winner = ""; // For later winner return
   // Winner outcome
   if (humanScore > machineScore) {
-    console.log(
-      `\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`
-    );
+    console.log(`\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`);
     return (winner = "The winner is the PLAYER");
   } else if (humanScore < machineScore) {
-    console.log(
-      `\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`
-    );
+    console.log(`\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`);
     return (winner = "The winner is the COMPUTER");
   } else if (humanScore === machineScore) {
-    console.log(
-      `\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`
-    );
+    console.log(`\n\tGAME SCORE\n\nHuman: ${humanScore}\nMachine: ${machineScore}\n\n`);
     return (winner = "This game ended up a DRAW");
   }
 }
